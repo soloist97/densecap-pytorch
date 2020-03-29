7c5cb92d3245a020defdfc5ca0540048d8c7e1fe
soloist97 <1069023401@qq.com>
2020-03-29 16:26:51 +0800

    add pytorch detection reference code

---
+++ detection_utils/__init__.py
@@ -0,0 +1,7 @@
+"""
+Follow the tutorial: https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html#torchvision-object-detection-finetuning-tutorial
+
+Download from https://github.com/pytorch/vision/tree/master/references/detection
+
+Specific Version d2c763e14efe57e4bf3ebf916ec243ce8ce3315c
+"""
